f827a93d26e29a415e5e06ebcaec6e6e8e03fb6b
rebeckawinqvist <rebecka.w@telia.com>
2020-02-18 09:49:17 +0100

    -

--- cvxpy examples/problem4.py
+++ cvxpy examples/problem4.py
@@ -17,10 +17,13 @@ g_t = torch.randn(m, 1, requires_grad=True)
 lambd_t = torch.rand(1, 1, requires_grad=True)
 print("F_t grad: ", F_t.grad)
 print("g_t grad: ", g_t.grad)
+
 # Canonicalizes problem to extract C and R
 layer = CvxpyLayer(problem, parameters=[F, g, lambd], variables=[x])
 # Calling layer applies the map R o s o C, returning a solution to the problem
+
 x_star, = layer(F_t, g_t, lambd_t)
+
 # Computes the gradients of summing x_star, with respect to F_t, g_t, lambd_t
 x_star.sum().backward()
 print("F_t grad: ", F_t.grad)
